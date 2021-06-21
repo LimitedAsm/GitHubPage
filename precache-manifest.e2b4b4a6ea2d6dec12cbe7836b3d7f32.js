8a96ac304d2a997440e9b62a2a2de6e5172ced1b
LimitedAsm <48093491+LimitedAsm@users.noreply.github.com>
2021-06-21 09:39:48 +0300

    init

---
+++ precache-manifest.e2b4b4a6ea2d6dec12cbe7836b3d7f32.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5694e719c2717f3e6f6",
+    "url": "/css/app.04962d88.css"
+  },
+  {
+    "revision": "87f01332fcf98352fac1dad13dad35d1",
+    "url": "/img/account_circle_white_24dp.87f01332.svg"
+  },
+  {
+    "revision": "51009d71d7408cfd1545e01266215e0c",
+    "url": "/img/date_range_white_24dp.51009d71.svg"
+  },
+  {
+    "revision": "a942a43639628e157848d20046aa3eeb",
+    "url": "/img/highlight_off_black_24dp.a942a436.svg"
+  },
+  {
+    "revision": "5787ef1e1058dca4d94b9bc01441e1fe",
+    "url": "/img/visibility_black_24dp.5787ef1e.svg"
+  },
+  {
+    "revision": "7f0a7450fe514e4284bf33f3a819acb0",
+    "url": "/img/visibility_off_black_24dp.7f0a7450.svg"
+  },
+  {
+    "revision": "f1b6c52032ac52fc65a2c7f7c16d9c46",
+    "url": "/img/window_white_24dp.f1b6c520.svg"
+  },
+  {
+    "revision": "ab172f52e3ede0015011841c426f7b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5694e719c2717f3e6f6",
+    "url": "/js/app.846f876e.js"
+  },
+  {
+    "revision": "0722c255e21f19fdc0b4",
+    "url": "/js/chunk-vendors.ddfc98fd.js"
+  },
+  {
+    "revision": "2c653d07d2247db054e3c0f4505dbb67",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
